8af8c79064f1a699d604d1391ad38ef9be446c35
guzmanpdro <pedroguzman2598@gmail.com>
2024-07-19 00:01:43 -0400

    Add list of countries and their styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,40 @@
 import Container from "./ui/container";
-import ListOfCountries from "./ui/list-of-countries/listOfCountries";
+import CountryCard from "./ui/country-card/countryCard";
 import styles from "./page.module.css";
 import { countries as initialCountries } from "./lib/data.json"
 
 export default function Home() {
   return (
-    <main>
+    <main className={styles.main}>
       <Container>
-        <ListOfCountries countries={initialCountries} />
+        {
+          <ul className={styles.countryList}>
+            {
+              initialCountries.slice(0, 10).map((country) => {
+                const {
+                  alpha3Code,
+                  capital,
+                  flags,
+                  name,
+                  population,
+                  region
+                } = country
+      
+                return (
+                  <li key={alpha3Code} className={styles.listItem}>
+                    <CountryCard 
+                      capital={capital || "NA"}
+                      flags={flags}
+                      name={name}
+                      population={population}
+                      region={region}
+                    />
+                  </li>
+                )
+              })
+            }
+          </ul>
+        }
       </Container>
     </main>
   );
