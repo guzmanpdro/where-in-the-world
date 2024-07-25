62722a2d46af97ef091612bd7a45acaec2982527
guzmanpdro <pedroguzman2598@gmail.com>
2024-07-25 11:34:33 -0400

    Add the actionsBar component

---
+++ app/ui/actions-bar/actionsBar.tsx
@@ -0,0 +1,21 @@
+import SearchIcon from "../icon"
+import styles from "./actionsBar.module.css"
+
+export default function ActionsBar() {
+  return (
+    <section className={styles.actionsBar}>
+      <form>
+        <label className={styles.labelSeach}>
+          <input
+            className={styles.inputSearch}
+            type="search"
+            placeholder="Search for a country..." 
+          />
+          <div className={styles.searchIcon}>
+            <SearchIcon />
+          </div>
+        </label>
+      </form>
+    </section>
+  )
+}
